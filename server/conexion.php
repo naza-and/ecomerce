6df813e3fc9f190409677dee25fb5db015d5f8f5
Changorius <lucasromeroyacurto@gmail.com>
2021-08-18 19:26:46 -0300

    aa

--- server/conexion.php
+++ server/conexion.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_conexion = "localhost";
-$database_conexion = "ecomerce";
-$username_conexion = "root";
-$password_conexion = "";
-$conexion = mysql_pconnect($hostname_conexion, $username_conexion, $password_conexion) or trigger_error(mysql_error(),E_USER_ERROR); 
+$hostname = "localhost";
+$database = "ecomerce";
+$username = "root";
+$password = "";
+$conexion = NEW mysql($hostname, $username, $password, $database) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
