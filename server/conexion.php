1c3371314291fec91b9ce77e79911ede83aa442f
Nazareno <naza100.es@gmail.com>
2021-09-22 19:10:31 -0300

    primer push

---
+++ server/conexion.php
@@ -0,0 +1,11 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_conexion = "localhost";
+$database_conexion = "ecomerce";
+$username_conexion = "root";
+$password_conexion = "";
+$conexion = new mysqli($hostname_conexion, $username_conexion, $password_conexion, $database_conexion) or trigger_error(mysql_error(),E_USER_ERROR); 
+>>>>>>> fd7a8a5c90b84b3e2ef3fd056286af0de4b013d9
+?>
---
+++ server/models/productos.php
@@ -0,0 +1,17 @@
+<?php
+
+function mostrar_productos() {
+    include("conexion.php");
+    $query = $conexion->query("select descripcion from productos");
+    $resultado = $query->fetch_all(1);
+    return $resultado;
+}
+//consulta de categorias de juego
+
+function mostrar_categorias() {
+    $query = $conexion->query("select productos.nombre,productos.descripcion, productos.precio, productos.cantidad  
+    from productos
+    inner join categoria on categoria.id = id_categorias WHERE categoria.nombre=´aventura´");
+    $resultado = $query->fetch_all(1);
+    return $resultado;
+}
